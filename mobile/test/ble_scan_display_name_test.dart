7bf08b18927f56f02c1904ffcb9cb08486b1656f
Charles Allen <cwalle@gmail.com>
2026-06-26 10:12:47 -0400

    Fix Meshtastic BLE scan names

--- mobile/test/ble_scan_display_name_test.dart
+++ mobile/test/ble_scan_display_name_test.dart
@@ -1,7 +1,10 @@
+import 'package:aervyx_mobile/services/api_service.dart';
 import 'package:aervyx_mobile/services/ble_service.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
   group('meshtasticScanDisplayName', () {
     test('prefers platform name', () {
       expect(
@@ -34,6 +37,32 @@ void main() {
         ),
         'Meshtastic device (AA:BB:CC:DD:EE:FF)',
       );
+    });
+  });
+
+  group('saved BLE reconnect name', () {
+    test('updates from current device short name', () async {
+      final ble = BleService(ApiService())
+        ..debugSetSavedBleReconnectTarget(
+          remoteId: 'AA:BB:CC:DD:EE:FF',
+          name: 'OldName',
+        );
+
+      await ble.debugRefreshSavedBleReconnectName(' NewShort ');
+
+      expect(ble.savedBleDeviceName, 'NewShort');
+    });
+
+    test('ignores blank short names', () async {
+      final ble = BleService(ApiService())
+        ..debugSetSavedBleReconnectTarget(
+          remoteId: 'AA:BB:CC:DD:EE:FF',
+          name: 'KeepMe',
+        );
+
+      await ble.debugRefreshSavedBleReconnectName(' ');
+
+      expect(ble.savedBleDeviceName, 'KeepMe');
     });
   });
 }
